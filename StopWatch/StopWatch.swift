7c7f86ae402b5137fbf54430faf5d9b036a3c9da
Maribel Montejano <outdoorsole@users.noreply.github.com>
2017-10-24 18:43:10 -0700

    Create Stopwatch class; add 2 methods (start & stop)

---
+++ StopWatch/StopWatch.swift
@@ -0,0 +1,22 @@
+//
+//  StopWatch.swift
+//  StopWatch
+//
+//  Created by Maribel Montejano on 10/24/17.
+//  Copyright © 2017 Maribel Montejano. All rights reserved.
+//
+
+import Foundation
+
+class StopWatch {
+    var startTime: Date?
+    
+    func start() {
+        // Record the current time to startTime
+        startTime = Date()
+    }
+    
+    func stop() {
+        
+    }
+}
