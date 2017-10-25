c8d0397cea9aabc1dcdc18034c0525f1eccfc68b
Maribel Montejano <outdoorsole@users.noreply.github.com>
2017-10-24 19:12:25 -0700

    Add IBActions for stop/start buttons; IBOutlet for timerLabel

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var timerLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startButton(_ sender: UIButton) {
+        print("start button tapped")
+    }
+    
+    @IBAction func stopButton(_ sender: UIButton) {
+        print("stop button tapped")
+    }
 
 }
 
